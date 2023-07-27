7c5ac7f62f96f9d2420d51d7775292c21b335b03
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-07-27 11:48:05 +0530

    added

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+
+const strong = document.getElementsByTagName("strong");
+const anchor = document.getElementsByTagName("a")[0];
+
+// anchor.addEventListener("onMouseOver", highlight());
+// anchor.addEventListener("onMouseLeave", return_normal());
+
 function highlight() {
     //Write your code here
+    // strong.style.color = 'green';
 
-
+    for (let i = 0; i < strong.length; i++) {
+        // console.log(strong[i].innerText);
+        strong[i].style.color = 'green';
+    }
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    // anchor.removeEventListener("onMouseOver", highlight());   
+    for (let i = 0; i < strong.length; i++) {
+        // console.log(strong[i].innerText);
+        strong[i].style.color = 'black';
+    } 
 }
